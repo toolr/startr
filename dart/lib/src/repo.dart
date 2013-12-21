953d9601a23c5ca41cce8592deb9af26d9fa1d61
damondouglas <douglas.damon@gmail.com>
2013-12-21 14:19:56 -0500

    Fixed dart codec errors

--- dart/lib/src/repo.dart
+++ dart/lib/src/repo.dart
@@ -13,6 +13,65 @@ class Repo {
 		return _process('git', ['clone',this._url, '.'],'cloning repo');
 	}
 
+	Future build_startr(){
+		var completer = new Completer();
+
+		var dir = new Directory('.git');
+
+		 if(dir.existsSync()){
+		 	dir.deleteSync(recursive:true);
+		 }
+
+		dir = new Directory('.');
+		dir.list(recursive:true)
+		.forEach((FileSystemEntity fse){
+			if(fse is File){
+				var fh = new FileHandler(fse);
+				fh.rename(_args)
+				.then((file){
+					fh = new FileHandler(file);
+					// var completer = new Completer();
+					// completer.complete(file);
+					// return completer.future;
+					return fh.modify(_args);
+				})
+				.then((file) => print("Processed: ${file.path}"));
+			}
+		});
+		completer.complete();
+		return completer.future;
+	}
+
+	Future _process(String command, List<String> args, String message){
+		return Process.start(command, args)
+	      //.then((_) => print('$message...'))
+	      .then((int exitCode) => exitCode);
+	}
+}
+
+class FileHandler {
+	File _file;
+
+	FileHandler(this._file);
+
+	Future rename(List<String> args){
+		return _replace(_file.path, args)
+		.then((path) => _file.rename(path));
+	}
+
+	Future modify(List<String> args) {
+		return _read()
+		.then((data){
+			if(data is List<int>) return _file.writeAsBytes(data);
+			if(data is String){
+				var completer = new Completer();
+				_replace(data, args)
+				.then((data) => completer.complete(_file.writeAsString(data)));
+				return completer.future;
+			}
+		});
+	}
+
 	Future<String> _replace(String text, List<String> args){
 		var completer = new Completer();
 
@@ -30,34 +89,17 @@ class Repo {
 		return completer.future;
 	}
 
-	Future build_startr(){
-		var completer = new Completer();
+	Future _read(){
+		String mimetype = mime.lookupMimeType(_file.path);
 
-		var dir = new Directory('.git');
-
-		 if(dir.existsSync()){
-		 	dir.deleteSync(recursive:true);
-		 }
-
-		dir = new Directory('.');
-		dir.list(recursive:true)
-		.forEach((FileSystemEntity fse){
-			if(fse is File){
-				_replace(fse.path, _args)
-				.then((path) => fse.renameSync(path))
-				.then((fse) => fse.readAsString())
-				.then((data) => _replace(data, _args))
-				.then((data) => fse.writeAsString(data))
-				.then((_) => print("Processed: ${fse.path}"));
-			}
-		});
-		completer.complete();
-		return completer.future;
-	}
-
-	Future _process(String command, List<String> args, String message){
-		return Process.start(command, args)
-	      //.then((_) => print('$message...'))
-	      .then((int exitCode) => exitCode);
+		if(mimetype != null) {
+			if(mimetype.contains('image')){
+				return _file.readAsBytes();
+			} else {
+				return _file.readAsString();
+			}	
+		} else {
+			return _file.readAsString();
+		}
 	}
 }
--- dart/lib/startr.dart
+++ dart/lib/startr.dart
@@ -3,5 +3,6 @@ library startr;
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
+import 'package:mime/mime.dart' as mime;
 
 part 'src/repo.dart';
